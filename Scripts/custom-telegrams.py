9ea19cc253959377322c1fefde8601b61a81cf18
Mustafa Adinata <adin97069@gmail.com>
2026-05-15 12:43:51 +0700

    Update custom-telegrams.py

--- Scripts/custom-telegrams.py
+++ Scripts/custom-telegrams.py
@@ -27,10 +27,11 @@ src_ip = (
   or "Unknown"
 )
 
-msg = f"A Wazuh Alert (Level {alert_level})\n\n"
-msg += f" Agent: (agent_name}\n"
-msg += "IP: {}\n".format(src_ip) msg += f"+Time: {time_str}\n"
-msg += f"Description: (description}\n"
+msg = f"⚠️ Wazuh Alert (Level {alert_level})\n\n"
+msg += f" Agent: {agent_name}\n"
+msg += f"IP: {src_ip}\n"
+msg += f"Time: {time_str}\n"
+msg += f"Description: {description}\n"
 
 if description == "SSH Brute Force detection": 
 msg += "\n STATUS: IP BLOCKED BY IPTABLES"
